e457f5281608b4386b0e25f9f1f08f6d9ccfa7b6
Wahidin <izulwahidin@my.id>
2023-01-06 18:22:37 +0700

    slug2string variable fix

--- src/functions.jsx
+++ src/functions.jsx
@@ -43,8 +43,8 @@ export const getNextPrevPage = (current,max) => {
 export const slug2string = (slug) => {
     let words = slug.split('-');
   
-    for (var i = 0; i < words.length; i++) {
-        var word = words[i];
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
         words[i] = word.charAt(0).toUpperCase() + word.slice(1);
     }
     return words.join(' ');
